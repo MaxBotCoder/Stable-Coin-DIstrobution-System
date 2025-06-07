3bdaa0ddebc85f1c4fa1369fe2130aa197e61131
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-06-07 14:31:20 -0700

    Update code.sol

--- code.sol
+++ code.sol
@@ -1,80 +1,189 @@
 //SPDX-License-Identifier: MIT
-pramga solidity 0.8.0;
+pragma solidity 0.8.0;
 
-contract StableCoinOffering {
+contract DispenserTypeContract {
+	address public Admin;
+	uint public PriceOfItem;
+	
+	constructor(){
+		Admin = msg.sender;
+	}
+}
 
+contract DataStorageTypeContract {
 	address Admin;
 	
-	struct ListingInfo {
+	constructor(){
+		Admin = msg.sender;
+	}
+}
+
+contract DepositTypeContract {
+	address Admin;
+	uint public OverdrawAttemptLimit;
+	
+	mapping(address => uint) public TransactionNumber;
+	mapping(address => uint) public AmountToWithdrawOrInput;
+	mapping(address => uint) public AmountWithinTotaly;
+	mapping(address => uint) public Recipient;
+	mapping(address => uint) public TransactionNumber;
+	mapping(address => uint) public MyOverdrawAttempts;
+	mapping(address => bool) public MaximumOverdrawAttempts;
+ 	mapping(address => string) SpecificTransactionType;
+	
+	constructor(){
+		Admin = msg.sender;
+	}
+	
+	struct _SpecificTransaction {
+		
+		uint AmountStored;
+		string TransactionType;
+		uint QuantityTransfered;
+		address From;
+		address To;
+		uint TransactionNumber;
+	}
+	
+	mapping(address => mapping(uint => _SpecificTransaction) public SpecificTransaction;
+	
+	struct _CurrentTransaction {
+		
+		uint AmountStored;
+		string TransactionType;
+		uint QuantityTransfered;
+		address From;
+		address To;
+		uint TransactionNumber;
 		
 	}
 	
-	mapping(msg,sender => ListingInfo) public Listing;
+	mapping(address => _CurrentTransaction) public CurrentTransaction;
 	
-	struct PersonalPurchaseInfo {
+	function ContractConfiguration (uint _MaxOverdrawThreshold) {
+		require(msg.sender == Admin);
+		OverdrawAttemptLimit = _MaxOverdrawThreshold;
 	}
 	
-	mapping(msg.sender => PersonalPurchaseInfo) public PurchasesMade;
-	
-	contructor(){
-		Admin = msg.sender;
+	function (string _TransactionType, uint _QuantityInOrOut, address _PossibleRecipient) payable public {
+		require(_TransactionType == "Deposit" || _TransactionType == "deposit" || _TransactionType == "Withdraw" || _TransactionType == "withdraw", "Not an option.");
+		
+		if(TransactionType == "Deposit" || TransactionType == "deposit"){
+		
+			AmountToWithdrawOrInput[msg.sender] = _QuantityInOrOut;
+			require (AmountToWithdrawOrInput[msg.sender] > CurrentTransition[msg.sender].AmountStored);
+			TransactionNumber[msg.sender] += 1;
+			Recipient[msg.sender] = _PossibleRecipient;
+			SpecificTransactionType[msg.sender] = _TransactionType;
+			AmountWithinTotaly[msg.sender] += AmountToWithdrawOrInput[msg.sender];
+			CurrentTransaction[msg.sender] = _CurrentTransaction( AmountWithinTotaly[msg.sender], SpecificTransactionType[msg.sender], AmountToWithdrawOrInput[msg.sender], msg.sender, Recipient[msg.sender], TransactionNumber[msg.sender]);
+			SpecificTransaction[msg.sender][TransactionNumber[msg.sender]] = CurrentTransaction[msg.sender];
+			
+		} else if (TransactionType == "Withdraw" || TransactionType == "withdraw") {
+			
+			if(CurrentTransaction[msg.sender].AmountStored < AmountToWithdrawOrInput[msg.sender]) {
+				MyOverdrawAttempts[msg.sender] += 1;
+			}
+			
+			if(MyOverdrawAttempts[msg.sender] < OverdrawAttemptLimit) {
+			
+				require(CurrentTransaction[msg.sender].AmountStored >= AmountToWithdrawOrInput[msg.sender], "Overdraw attempts are recorded, do not hit the limit threshold or further attempts will be punished with a gas penalty.");
+				payable(_PossibleRecipient).call{value: _QuantityInOrOut}("");
+				TransactionNumber[msg.sender] += 1;
+				Recipient[msg.sender] = _PossibleRecipient;
+				SpecificTransactionType[msg.sender] = _TransactionType;
+				AmountWithinTotaly[msg.sender] += AmountToWithdrawOrInput[msg.sender];
+				CurrentTransaction[msg.sender] = _CurrentTransaction( AmountWithinTotaly[msg.sender], SpecificTransactionType[msg.sender], AmountToWithdrawOrInput[msg.sender], msg.sender, Recipient[msg.sender], TransactionNumber[msg.sender]);
+			SpecificTransaction[msg.sender][TransactionNumber[msg.sender]] = CurrentTransaction[msg.sender];
+			
+			} else if (OverdrawAttemptLimit == MyOverdrawAttempts[msg.sender]) {
+			
+				assert(CurrentTransaction[msg.sender].AmountStored >= AmountToWithdrawOrInput[msg.sender]);
+				payable(_PossibleRecipient).call{value: _QuantityInOrOut}("");
+				TransactionNumber[msg.sender] += 1;
+				Recipient[msg.sender] = _PossibleRecipient;
+				SpecificTransactionType[msg.sender] = _TransactionType;
+				AmountWithinTotaly[msg.sender] += AmountToWithdrawOrInput[msg.sender];
+				CurrentTransaction[msg.sender] = _CurrentTransaction( AmountWithinTotaly[msg.sender], SpecificTransactionType[msg.sender], AmountToWithdrawOrInput[msg.sender], msg.sender, Recipient[msg.sender], TransactionNumber[msg.sender]);
+			SpecificTransaction[msg.sender][TransactionNumber[msg.sender]] = CurrentTransaction[msg.sender];
+			
+			}
+		}
 	}
-	
-	function AlterStableCoinOffering() {
-	
-	}
-	
-	function InvestInStableCoin() public {
-	
-	}
-	
 }
 
-contract LoanOffering {
+contract ContractMaker {
 
-	address Admin;
+	address Administrator;
+	bool HaveIMadeAContractYet = false;
 	
-	struct ListingInfo {
+	constructor(){
+		creator = msg.sender;
 	}
 	
-	mapping(msg,sender => ListingInfo) public Listing; 	
+	DispenserTypeContract public DTC;
+	DataStorageTypeContract public DSC;
+	DepositTypeContract public DPC;
+	UserEndContract public UserSideContract;
 	
-	struct PersonalPurchaseInfo {
+	mapping (address => uint) public ContractNumber;
+	mapping (address => mapping(string => mapping(uint => DispenserTypeContract))) public DespenserContract;
+	mapping (address => mapping(string => mapping(uint => DataStorageTypeContract))) public DataStorageContract;
+	mapping (address => mapping(string => mapping(uint => UserSideContract))) public DepositContract; 
+	
+	struct CommandList {
 	}
 	
-	mapping(msg.sender => PersonalPurchaseInfo) public PurchasesMade;
+	CommandList public Commands;
 	
-	contructor(){
-		Admin = msg.sender;
+	struct ContractModData {
+		string _TypeOfContract;
+		uint _AmountOfModifications;
+		uint _MaximumOverDrawWarning;
+		uint _ModificationTimeStamp;
+		uint _FailedModifications;
+		event _ErrorCode(string Error);
 	}
 	
-	function AlterLoanAttributes(){
+	mapping (address => mapping(string => mapping(uint => ContractModData))) public ContractModificationData; 
 	
+	function CreateContracts (string ContractType, string memory ContractName) public {
+		require(msg.sender == Administrator, "You do not have permissions to this smart contract.");
+		require(ContractType == "DispenserContract" || ContractType == "dispensercontract" || ContractType == "DataContract" || ContractType == "datacontract" || ContractType == "DepositContract" || ContractType == "depositcontract", "Invalid configuration.");
+		if(ContractType == "DispenserContract" || ContractType == "dispensercontract") {
+			ContractNumber[msg.sender] += 1;
+			DespenserContract[msg.sender][ContractName][ContractNumber[msg.sender]] = DTC = new DispenserTypeContract();
+			HaveIMadeAContractYet = true;
+			ContractModificationData[msg.sender][ContractNumber[msg.sender]][ContractName] = ("Dispenser", 0, 0);
+		} else if (ContractType == "DataContract" || ContractType == "datacontract") {
+		        ContractNumber[msg.sender] += 1;
+		        DataStorageContract[msg.sender][ContractName][ContractNumber[msg.sender]] = DSC = new DataStorageTypeContract();
+			HaveIMadeAContractYet = true;
+			ContractModificationData[msg.sender][ContractNumber[msg.sender]][ContractName] = ("Dispenser", 0, 0);
+		} else if (ContractType == "DepostContract" || ContractType == "depositcontract") {
+			ContractNumber[msg.sender] += 1;
+			DepositContract[msg.sender][ContractName][ContractNumber[msg.sender]] = DPC = new DepositTypeContract();
+			HaveIMadeAContractYet = true;
+			ContractModificationData[msg.sender][ContractNumber[msg.sender]][ContractName] = ("Dispenser", 0, 0);
+		}
+	}	
+	
+	function AdminControllPanel(string ContractType, string contractname , uint ContractNumber, uint ModificationNumber1) public {
+		require(msg.sender == Administrator, "You do not have permissions to this smart contract.");
+		if(ContractType == "DispenserContract" || ContractType == "dispensercontract"){
+		} else if (ContractType == "DataContract" || ContractType == "datacontract") {
+		} else if (ContractType == "DepostContract" || ContractType == "depositcontract") {
+		//Can only alter current contract data.
+			try DataStorageTypeContract.ContractConfiguration(MaximumOverdrawWarnings){
+				ContractModificationData[msg.sender][ContractName][ContractNumber[msg.sender]]._TypeOfContract = "Deposit Type Contract";
+				ContractModificationData[msg.sender][ContractName][ContractNumber[msg.sender]]._AmountOfModifications += 1;
+				contractModificationData[msg.sender][ContractName][ContractNumber[msg.sender]]._MaximumOverDrawWarning = ModificationNumber1;
+				contractModificationData[msg.sender][ContractName][ContractNumber[msg.sender]]._ModificationTimeStamp = block.timestamp;
+			} catch (string memory ErrorCode) {
+			 	emit contractModificationData[msg.sender][ContractName][ContractNumber[msg.sender]]._ErrorCode(ErrorCode);
+			 	contractModificationData[msg.sender][ContractName][ContractNumber[msg.sender]]._FailedModifications += 1;
+			}
+		}
 	}
 	
-	function GetLoan () public {
-	
-	}
-	
-}
-
-contract Administrator {
-	
-	address Admin;
-	
-	constructor () {
-		Admin = msg.sender;
-	}
-	
-	struct ContractHosted {
-	}
-	
-	mapping(address => ContractHosted) public ListingOrLoanInfo;
-	
-	function CreateLoanOrListing (String _ContratType) public {
-		require(_ContractType = "Stable Coin" || "StableCoin" || "Loan" || "loan", "Invalid Request.");
-		if(_ContractType = "Stable Coin" || "StableCoin") {
-		} else if (_ContractType = "Loan" || "loan") {
-		}	 
-	}
 }
